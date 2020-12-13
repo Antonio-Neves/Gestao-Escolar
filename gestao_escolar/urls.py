75ba002b37579450db531245837a7e34f994b6f0
Antonio <it-an@hotmail.com>
2020-12-13 15:40:30 -0300

    first commit

--- gestao_escolar/urls.py
+++ gestao_escolar/urls.py
@@ -1,5 +1,4 @@
 """gestao_escolar URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 """
