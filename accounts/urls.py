de3d3d23e43f2944becdff5b4034c6814fdac87c
Antonio <it-an@hotmail.com>
2021-01-16 10:47:06 -0300

    Accounts App

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,15 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 from accounts.views import (
 	UserLogin,
 	UserCreate,
 	UserChange,
 	PasswordChange,
+	UserDelete,
 	# PasswordReset,
 	# PasswordResetConfirm,
 	# PasswordResetComplete,
-	UserDelete
+
 )
 
 from django.contrib.auth.views import (
@@ -20,11 +21,11 @@ urlpatterns = [
 	path('login/', UserLogin.as_view(), name='login'),
 	path('user-new/', UserCreate.as_view(), name='user-new'),
 	path('<int:pk>/update/', UserChange.as_view(), name='user-change'),
+	path('<int:pk>/delete/', UserDelete.as_view(), name='user-delete'),
 	path('password-change/', PasswordChange.as_view(), name='password-change'),
-	# path('password-reset/', PasswordReset.as_view(), name='password-reset'),
 	path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+	path('', include('django.contrib.auth.urls')),
+	# path('password-reset/', PasswordReset.as_view(), name='password-reset'),
 	# path('reset/<uidb64>/<token>/', PasswordResetConfirm.as_view(), name="password_reset_confirm"),
 	# path('reset/done/', PasswordResetComplete.as_view(), name='password-reset-complete'),
-	path('<int:pk>/delete/', UserDelete.as_view(), name='user-delete'),
-	path('', include('django.contrib.auth.urls')),
 ]
