27a575cd5253ab004ab0be69d2b26262de34d271
antonio <it-an@hotmail.com>
2021-03-03 12:02:10 -0300

    Disciplinas sidebar se

--- principal/urls.py
+++ principal/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
 	path('ano-escolar-novo/', AnoEscolarNewView.as_view(), name='ano-escolar-novo'),
 	path('anos-letivos/', AnoLetivoListView.as_view(), name='anos-letivos'),
 	path('disciplina-nova/', DisciplinaNewView.as_view(), name='disciplina-nova'),
-	path('<int:pk>/alterar', DisciplinaUpdateView.as_view(), name='disciplinas-update'),
+	path('<int:pk>/alterar', DisciplinaUpdateView.as_view(), name='disciplina-update'),
 	path('disciplinas/', DisciplinaListView.as_view(), name='disciplinas'),
 ]
