a1d815a9b182a9e49c0e777a92167565a59baf70
antonio <it-an@hotmail.com>
2021-03-06 07:33:35 -0300

    Turma model

--- turmas/models.py
+++ turmas/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+from principal.models import AnoEscolar
+
 
 class Turma(models.Model):
-	pass
+
+	turma_id = models.AutoField(
+		primary_key=True
+	)
+	turma_ano_escolar = models.ForeignKey(
+		AnoEscolar,
+		verbose_name='Ano Escolar',
+		on_delete=models.DO_NOTHING,
+		related_name='anoescolar'
+
+	)
+
+
