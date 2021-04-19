7ef8bbe3b257a51ef72b96edb6717e0fb153ce28
antonio <it-an@hotmail.com>
2021-04-19 19:41:53 -0300

    Extra context for 'Aluno' search.

--- secretaria/views.py
+++ secretaria/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render, reverse
+from django.db.models import Case, CharField, Value, When
 
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView
@@ -40,8 +41,33 @@ class SecretariaSearchView(BaseAdminUsersSe, ListView):
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
-		turmas = Turma.objects.all().filter(
-			turma_ano_escolar__ano_escolar_etapa__etapa_basica_ano__ano_letivo_nome=CURRENT_YEAR
+
+		turmas = Turma.objects.filter(
+			turma_ano_letivo=CURRENT_YEAR
+		).annotate(
+			ano_escolar_display=Case(
+				When(turma_ano_escolar='CR', then=Value('Creche')),
+				When(turma_ano_escolar='G1', then=Value('Maternal I')),
+				When(turma_ano_escolar='G2', then=Value('Maternal II')),
+				When(turma_ano_escolar='G3', then=Value('Maternal III')),
+				When(turma_ano_escolar='G4', then=Value('Jardim I')),
+				When(turma_ano_escolar='G5', then=Value('Jardim II')),
+				When(turma_ano_escolar='1A', then=Value('1º Ano')),
+				When(turma_ano_escolar='2A', then=Value('2º Ano')),
+				When(turma_ano_escolar='3A', then=Value('3º Ano')),
+				When(turma_ano_escolar='4A', then=Value('4º Ano')),
+				When(turma_ano_escolar='5A', then=Value('5º Ano')),
+				When(turma_ano_escolar='6A', then=Value('6º Ano')),
+				When(turma_ano_escolar='7A', then=Value('7º Ano')),
+				When(turma_ano_escolar='8A', then=Value('8º Ano')),
+				When(turma_ano_escolar='9A', then=Value('9º Ano')),
+				output_field=CharField()
+			)
+		).values_list(
+			'ano_escolar_display',
+			'turma_nome',
+			'turma_etapa_basica',
+			'turma_aluno'
 		)
 
 		context['turmas'] = turmas
