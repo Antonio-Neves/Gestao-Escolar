c1e61182a02f1d6a7396e92eb2b39950bedb8ffa
antonio <it-an@hotmail.com>
2021-05-21 11:56:48 -0300

    Changed 'STATIC_URL' and 'MEDIA_URL'

--- gestao_escolar/settings.py
+++ gestao_escolar/settings.py
@@ -194,8 +194,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static03/'
+MEDIA_URL = 'media03/'
 
 # --- development --- #
 if DEBUG:
