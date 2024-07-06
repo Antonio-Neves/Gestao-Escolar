8426a7b02871237413eccbc6fc67a77991e8d188
Antonio Neves <antoniocalheirosneves@hotmail.com>
2024-07-06 20:04:29 +0100

    Changed default autofield for new django versions

--- gestao_escolar/settings.py
+++ gestao_escolar/settings.py
@@ -276,4 +276,7 @@ MESSAGE_TAGS = {
 }
 
 # ----------------------------------------------------------
-# DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
